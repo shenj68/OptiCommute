72c13c205d51fe088c80d4e6504d2f85ed254dd5
Derek Li <derekli33@hotmail.com>
2024-12-26 20:46:04 -0500

    new

---
+++ frontend/src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import VideoPlayer from "./components/videoplayer";
+
+export default function Dashboard() {
+  const videoSrc = "/video"; // Replace with your video path
+
+  return (
+    <div className="flex flex-col justify-start items-start min-h-screen text-center">
+      <div className="w-full max-w-3xl p-4 mt-[5%] ml-[10%]">
+        <VideoPlayer videoSrc={videoSrc} />
+      </div>
+    </div>
+  );
+}
