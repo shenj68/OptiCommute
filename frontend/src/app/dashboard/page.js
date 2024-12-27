d37a5bb41f46738615d1ba5ac90ad97ab345fd30
Derek Li <derekli33@hotmail.com>
2024-12-26 21:04:28 -0500

    dummy vid

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -5,7 +5,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-col justify-start items-start min-h-screen text-center">
-      <div className="w-full max-w-3xl p-4 mt-[5%] ml-[10%]">
+      <div className="w-full max-w-5xl p-4 mt-[2%] ml-[2%]">
         <VideoPlayer videoSrc={videoSrc} />
       </div>
     </div>
